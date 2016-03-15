7fe5197371affc63a9db5fb99ba677c165451104
Roberto Grassi <roberto.grassi@live.com>
2016-03-15 13:13:01 +0100

    added javascript

---
+++ src/WebHost/Startup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(WebHost.Startup))]
+namespace WebHost
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+        }
+    }
+}
