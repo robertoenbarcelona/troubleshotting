17f3e98a31af9d19c7c66098f4cdee6051276aef
Roberto Grassi <roberto.grassi@live.com>
2016-03-16 15:37:38 +0100

    test

--- src/WebHost/Controllers/TestJsController.cs
+++ src/WebHost/Controllers/TestJsController.cs
@@ -1,16 +1,19 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-
+﻿
 namespace WebHost.Controllers
 {
+    using System.Web.Mvc;
+
     public class TestJsController : Controller
     {
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public ActionResult LogIIS()
+        {
+            throw new System.Exception("Test de excepciones no gestionadas");
         }
     }
 }
